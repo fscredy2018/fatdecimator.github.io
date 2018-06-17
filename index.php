d323b296f22dd7f02086409defe2885ff9f707f4
Fred <fscredy2018@gmail.com>
2018-06-17 08:48:41 +0200

    first

---
+++ index.php
@@ -0,0 +1,119 @@
+﻿<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <title>Doctors Are Shocked: New Natural Method Reverses Weight Gain Without Drugs Or Surgery!</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta charset="UTF-8">
+        <meta NAME="robot" CONTENT="index, follow">
+	      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+        <link href="https://fonts.googleapis.com/css?family=Oswald:300,700|Roboto:300,300i,900,900i" rel="stylesheet">
+        <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+
+    <body>
+      <section class="fd-header2 pt-4">
+        <div class="container">
+          <div class="row pb-3">
+            <div class="col-md-8 offset-md-2">
+              <h2 class="text-center text-white mb-3">FLAT BELLY BREAKTHROUGH!</h2>
+              <p class="text-center text-white fd-14px"><script type="text/javascript">
+                var date = new Date();
+                var month = new Array(7);
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                var year = date.getYear();
+                if (year < 2000) { year+=1900; }
+                document.write(month[date.getMonth()] + " " + date.getDate() + ", " + year);
+                </script> <i class="pl-3 fa fa-eye" aria-hidden="true"></i> 1,429,273 Views <i class="pl-3 fa fa-heart" aria-hidden="true"></i> 586,112 Likes <i class="pl-3 fa fa-clock-o" aria-hidden="true"></i> Reading Time 5 min</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="container bg-primary rounded">
+          <div class="row">
+            <div class="col-12 pt-5 pl-md-5">
+              <img class="d-block fd-kyleStop2 img-fluid" src="assets/kyle-stop.png">
+
+              <a href="http://www.fatdecimator.com"><img class="float-md-left d-block m-auto img-fluid pb-3" src="assets/video-thumb.png"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="row pt-5 pb-5">
+            <div class="col-md-10 m-auto fd-whiteBg rounded p-3 p-md-5">
+              <h3 class="text-center mb-4"><a class="text-primary" href="http://www.fatdecimator.com"><span class="fd-pinkText">Doctors Are Shocked:</span> New Natural Method Reverses Weight Gain Without Drugs Or Surgery!</a></h3>
+
+              <hr class="mb-4">
+
+              <p class="text-center mb-5"><em>Article Summary</em></p>
+
+              <p class="fd-24px pl-md-5 pr-md-5"><b>(Try This Tonight):</b> This newly discovered technique actually "forces" your body to naturally reverse the effects of weight gain at its core.</p>
+
+              <p class="fd-24px pl-md-5 pr-md-5">Doctors all around the world are shocked since this technique does not involve taking drugs, surgery, dieting, gyms or pill supplements.</p>
+
+              <div class="row">
+                <div class="col-md-8 m-auto">
+                  <a href="http://www.fatdecimator.com" class="btn btn-lg btn-info btn-block p-4 text-uppercase hvr-grow-shadow fd-btnText2">watch the video now</a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="row pb-5">
+            <div class="col-md-10 m-auto">
+              <p class="text-center fd-grayText fd-14px">THIS IS AN ADVERTORIAL AND NOT AN ACTUAL NEWS ARTICLE, BLOG, OR CONSUMER PROTECTION UPDATE.</p>
+            </div>
+          </div>
+        </div>
+      </section>  
+        
+      <section class="fd-footer">
+        <div class="container">
+          <div class="row pt-5 pb-4">
+            <div class="col-md-8">
+             <p class="fd-footerText fd-14px mt-3"><a class="fd-footerText mr-2" href="http://www.fatdecimator.com/contact.html" target="_blank">Contact</a> | <a class="fd-footerText ml-2 mr-2" href="http://www.fatdecimator.com/terms.html" target="_blank">Terms of Service</a> | <a class="fd-footerText ml-2 mr-2" href="http://www.fatdecimator.com/privacy.html" target="_blank">Privacy</a> | <a class="fd-footerText ml-2" href="http://www.fatdecimator.com/affs" target="_blank">Affiliate Sign Up</a></p>
+
+              <p class="fd-footerText fd-14px">All Material Copyright © FatDecimator.com</p>
+            </div>
+            <div class="col-md-4">
+              <img class="d-block m-auto img-fluid" src="assets/logo-footer.png">
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <script>
+
+    var noTimeLeft  = "a few seconds";
+    var minutesTxt  = " minutes and ";
+    var secondsTxt  = " seconds";
+    var redirTime   = "28800";
+    var trackEvents = "1";
+
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="js/presell.min.js"></script>
+
+    <!--<script src="js/jquery.min.js"></script>-->
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
